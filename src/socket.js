1a7f4402bd817804dfb02fe7e5c6bf82e7e31a98
somnathbk62 <somnathbk62@gmail.com>
2025-07-23 14:10:12 +0530

    Fix socket.io options and add Netlify _redirects for React Router

--- src/socket.js
+++ src/socket.js
@@ -3,9 +3,9 @@ import { io } from "socket.io-client";
 export const initSocket = async () => {
   const options = {
     "force new connection": true,
-    reconnectionAttempt: "Infinity",
-    timeout: 10000,
-    transports: ["websocket"],
+    reconnectionAttempts: Infinity,
+    timeout: 20000,
+    transports: ["websocket", "polling"],
   };
   return io(process.env.REACT_APP_BACKEND_URL, options);
 };
